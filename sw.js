77d73ae2632f54c18778f954978bde57acad5577
mertkarababa-ae <73306634+mertkarababa-ae@users.noreply.github.com>
2026-03-14 11:40:41 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = 'arapca-v1';
+const ASSETS = ['/arapca-kartlar/', '/arapca-kartlar/index.html'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
